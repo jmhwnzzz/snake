fdb9c5b1e55fd2c4825ed1b787fdbfef0412923d
19521500 <71805459+jmhwnzzz@users.noreply.github.com>
2020-12-08 20:31:16 +0700

    Update snakegame.h

--- snakegame.h
+++ snakegame.h
@@ -1,9 +1,10 @@
-#pragma once // tránh đụng độ thư viện khi gọi chồng file lên nhau
+#pragma once 
 #include <stdio.h>
 #include <conio.h>
-#include<ctime> /* thư viện hỗ trợ về thời gian thực */
-#include "windows.h" // thư viện này bá đạo lắm nhé - chứa nhiều đồ chơi nek - cứ tìm hiểu dần dần s
-//======= lấy tọa độ x của con trỏ hiện tại =============
+#include<ctime> 
+#include "windows.h" 
+
+
 #define KEY_NONE	-1
 int whereX()
 {
@@ -12,7 +13,7 @@ int whereX()
 		return csbi.dwCursorPosition.X;
 	return -1;
 }
-//========= lấy tọa độ y của con trỏ hiện tại =======
+
 int whereY()
 {
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
@@ -20,7 +21,7 @@ int whereY()
 		return csbi.dwCursorPosition.Y;
 	return -1;
 }
-//============== dịch con trỏ hiện tại đến điểm có tọa độ (x,y) ==========
+
 void gotoXY(int x, int y)
 {
 	HANDLE hConsoleOutput;
@@ -28,7 +29,7 @@ void gotoXY(int x, int y)
 	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleCursorPosition(hConsoleOutput, Cursor_an_Pos);
 }
-//============= đặt màu cho chữ =========
+
 void SetColor(WORD color)
 {
 	HANDLE hConsoleOutput;
@@ -44,14 +45,14 @@ void SetColor(WORD color)
 
 	SetConsoleTextAttribute(hConsoleOutput, wAttributes);
 }
-//============== làm ẩn trỏ chuột ===========
+
 void ShowCur(bool CursorVisibility)
 {
 	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO cursor = { 1, CursorVisibility };
 	SetConsoleCursorInfo(handle, &cursor);
 }
-//======= trả về mã phím người dùng bấm =========
+
 int inputKey()
 {
 	if (_kbhit())
